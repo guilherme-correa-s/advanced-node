1883fca574c3315a93bc2c8daafcd12a068d043a
guilherme correa <guilhermescorrea@hotmail.com.br>
2022-07-01 15:39:40 -0300

    chore: add eslint

--- src/main/config/module-alias.ts
+++ src/main/config/module-alias.ts
@@ -1,4 +1,4 @@
-import { addAlias } from "module-alias"
-import { resolve } from "node:path"
+import { addAlias } from 'module-alias'
+import { resolve } from 'node:path'
 
-addAlias("@", resolve("dist"))
+addAlias('@', resolve('dist'))
