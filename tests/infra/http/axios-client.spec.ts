16f9267a73040f756e967d96f8c23c8f0ec3ee9b
guilherme correa <guilhermescorrea@hotmail.com.br>
2022-07-05 10:20:12 -0300

    ensure AxiosHttpClient rethrows if axios throws

--- tests/infra/http/axios-client.spec.ts
+++ tests/infra/http/axios-client.spec.ts
@@ -42,5 +42,12 @@ describe('AxiosHttpClient', () => {
       expect(result).toEqual('any_data')
       expect(fakeAxios.get).toHaveBeenCalledTimes(1)
     })
+
+    it('should rethrows ig get throws', async () => {
+      fakeAxios.get.mockRejectedValueOnce(new Error('http_error'))
+      const promise = sut.get({ url, params })
+
+      await expect(promise).rejects.toThrow(new Error('http_error'))
+    })
   })
 })
