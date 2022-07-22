b59900bdefb8e41d89b76b61c933960b93e32e2d
guilherme correa <guilhermescorrea@hotmail.com.br>
2022-07-22 19:39:01 -0300

    move JwtTokenGenerator to its owen file

--- tests/infra/crypto/jwt-token-generator.spec.ts
+++ tests/infra/crypto/jwt-token-generator.spec.ts
@@ -1,20 +1,11 @@
 import { TokenGenerator } from '@/data/contracts/crypto'
+import { JwtTokenGenerator } from '@/infra/crypto'
 
 import jwt from 'jsonwebtoken'
 
 jest.mock('jsonwebtoken')
-
-class JwtTokenGenerator {
-  constructor (private readonly secret: string) {}
-
-  async generateToken (params: TokenGenerator.Params): Promise<TokenGenerator.Result> {
-    const expirationInSeconds = params.expirationInMs / 1000
-    const token = jwt.sign({ key: params.key }, this.secret, { expiresIn: expirationInSeconds })
-    return token
-  }
-}
 describe('JwtTokenGenerator', () => {
-  let sut: JwtTokenGenerator
+  let sut: TokenGenerator
   let fakeJwt: jest.Mocked<typeof jwt>
 
   beforeAll(() => {
